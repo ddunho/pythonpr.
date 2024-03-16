3dc8f7ce8e17284076630927fbb07e0e62476b53
ddunho <dydkwogur@naver.com>
2024-03-16 19:24:05 +0900

    first commit

---
+++ 문자열 포맷.py
@@ -0,0 +1,15 @@
+python='파이썬' 
+java='자바'
+print('개발 언어에는 {},{} 등이 있어요'.format(python,java)) 
+"""변수 선언 방법
+   1. python,java = 파이썬,자바
+   2. python='파이썬'
+   java='자바'
+   """
+print('개발 언어에는',python,java,'등이 있어요')
+
+print('개발 언어에는 {0},{1} 등이 있어요'.format(python,java))
+#순서대로 변수가 들어감
+
+print(f'개발 언어에는 {python},{java} 등이 있어요')
+#개인적으로 가장 편리
---
+++ 파이썬 문자열 메소드.py
@@ -0,0 +1,6 @@
+letter='hELLO? lET ME INTRODUCE MY SELF. mY NAME IS sEUNGMIN jUNG.i AM 15 YEARS OLD. tHERE ARE...'
+print(letter.lower()) #모든 글자를 소문자로, 반대는 upper
+print(letter.capitalize()) #첫 글자만 대문자로, 나머지는 모두 소문자
+print(letter.swapcase()) #모든 문장에서의 대문자와 소문자를 뒤바꿔줌
+print(letter.title()) #각 단어들의 첫글자만 대문자로
+print(letter.split()) #띄어쓰기로 나누어진 단어를 리스트형태로 반환
