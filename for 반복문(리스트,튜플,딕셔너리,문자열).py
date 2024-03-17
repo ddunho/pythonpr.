c496d8ec03bcd1359409b4b893bb54afb928195e
ddunho <dydkwogur@naver.com>
2024-03-17 13:05:46 +0900

    first commit

---
+++ for 반복문(리스트,튜플,딕셔너리,문자열).py
@@ -0,0 +1,19 @@
+my_list = [1,2,3]
+for x in my_list: #for : 반복문 x : 변수
+    print(x)
+my_tuple=(1,2,3)
+for x in my_tuple:
+    print(x)
+
+person = {'이름': '나귀욤', '나이':7,'키':120,'몸무게':23} #딕셔너리는 key와 value가 쌍으로 이루어짐
+for v in person.values(): #value만 출력
+    print(v)
+for v in person.keys(): #key만 출력
+    print(v)
+
+fruit = 'apple'
+for x in fruit:
+    print(x)
+    
+
+
