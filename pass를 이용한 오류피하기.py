0b86668c4ee560b4bdde796fb2ea9382f8143438
ddunho <dydkwogur@naver.com>
2024-03-27 00:24:15 +0900

    first commit

---
+++ pass를 이용한 오류피하기.py
@@ -0,0 +1,9 @@
+class BlackBox:  #pass를 사용하여 마무리되지않는 코드에 pass를 쓰면, 오류없이 그냥 넘어감
+    def __init__(self):
+        pass
+    def record(self):
+        pass
+    def stop(self):
+        pass
+    def format(self):
+        pass
