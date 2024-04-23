e9c871b8565a9337462d15dbb6a59cf97711f64d
ddunho <dydkwogur@naver.com>
2024-04-24 00:42:57 +0900

    first commit

---
+++ 모듈.py
@@ -0,0 +1,7 @@
+import my_module
+my_module.price(3) # #3명이서 영화보러 갔을 때 가격
+my_module.price_morning(4) #4명이서 조조 할인 영화 보러 갔을때
+my_module.price_soldiers(5) #5명이서 군인이 영화를 보러 갔을 때
+
+#모듈을 호출하려면, 모듈이 현재 작업중인 파일과 동일한 디렉토리에 있거나,
+#파이썬이 모듈을 찾을수 있는 경로에 있어야함.
